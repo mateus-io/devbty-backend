a1df0e4bdc42d5c958f148cccf11a21a96995997
mateus-coder1 <letsgood56@gmail.com>
2022-11-05 01:35:30 -0300

    :wrench: config: postgresql database setup and prisma orm setup

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,27 @@
-import Fastify from "fastify"
+import Fastify from 'fastify'
+import { prisma } from './clients/Prisma'
+import cors from '@fastify/cors'
 
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
   })
-  await fastify.listen({ port: 4000 })
+
+  await fastify.register(cors, {
+    origin: true,
+  })
+
+  fastify.get('/users', async () => {
+    const users = await prisma.user.findMany({
+      where: {
+        name: {
+          startsWith: 'm',
+        },
+      },
+    })
+    return { users }
+  })
+  await fastify.listen({ port: 4000 /*host: '0.0.0.0' */ })
 }
 
 bootstrap()
