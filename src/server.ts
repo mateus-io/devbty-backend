e95dd6be34c48a54c473933cdb07a18dd1b51c30
mateus-coder1 <letsgood56@gmail.com>
2022-11-04 23:03:04 -0300

    chore: remove nodemon and include tsx

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,10 @@
-const test = "test"
-console.log(test)
+import Fastify from "fastify"
+
+async function bootstrap() {
+  const fastify = Fastify({
+    logger: true,
+  })
+  await fastify.listen({ port: 4000 })
+}
+
+bootstrap()
